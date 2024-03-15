4c30ed9fb23b71f02df908713ff7597e77d7528b
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-15 11:42:13 +0600

    While Loop

--- 01. Beginner/03. Loops and Strings/01. index.js
+++ 01. Beginner/03. Loops and Strings/01. index.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 5; i++) {
   console.log("Iteration: ", i);
 }
 
+
+
+
+// While Loop
+console.log("While Loop");
+
+let count = -3;
+while (count <= 3) {
+  console.log("Count: ", count);
+  count++; // Increment count
+}
