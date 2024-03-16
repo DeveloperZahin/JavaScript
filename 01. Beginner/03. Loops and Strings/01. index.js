6e6afdd81e47f91a9b9839ff2b0bda09f0bba76c
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-16 11:27:37 +0600

    String Methods in JS

--- 01. Beginner/03. Loops and Strings/01. index.js
+++ 01. Beginner/03. Loops and Strings/01. index.js
@@ -146,3 +146,49 @@ console.log(name[9]);
 const greeting = `Hello, ${name}!`;
 console.log(greeting); // Output: Hello, Mohammad Zahin!
 
+
+
+
+//////////////////////////////////////////////// String Methods in JS ////////////////////////////////////////////////
+// 1. toUpperCase() and toLowerCase() - These methods convert a string to all uppercase or lowercase letters, respectively.
+const message = "This is a message.";
+
+console.log(message.toUpperCase());             // Output: THIS IS A MESSAGE.
+console.log(message.toLowerCase());             // Output: this is a message.
+
+// 2. trim() - The trim() method removes leading and trailing whitespace characters (spaces, tabs, newlines) from a string.
+const messySentence = "  Extra spaces everywhere!  ";
+
+console.log(messySentence.trim());              // Output: Extra spaces everywhere!
+
+// 3. slice() - The slice() method extracts a section of a string and returns a new string. It takes two arguments
+const title = "JavaScript: The Definitive Guide";
+
+console.log(title.slice(0, 11));                // Output: JavaScript:
+console.log(title.slice(12));                   // Output: The Definitive Guide
+
+// 4. concat() - The concat() method joins two or more strings and returns a new string.
+const firstName = "John";
+const lastName = "Doe";
+
+console.log(firstName.concat(" ", lastName));   // Output: John Doe
+
+// 5. indexOf() and lastIndexOf() - These methods search for a substring within a string and return the index of the first or last occurrence, respectively. They return -1 if the substring is not found.
+const searchString = "apple is a fruit, but banana is not";
+
+console.log(searchString.indexOf("apple"));     // Output: 0
+console.log(searchString.lastIndexOf("apple")); // Output: 6
+
+// 6. replace() - The replace() method allows you to replace a part of a string with another string.
+const slogan = "JS: Making web development fun";
+
+console.log(slogan.replace("fun", "easier"));   // Output: JS: Making web development easier
+
+// 7. str.charAt(idx) - The charAt(idx) method returns a single character as a string, not the character's numerical code.
+const char = "Hello, world!";
+
+const firstChar = char.charAt(0); // H
+const fifthChar = char.charAt(4); // o
+
+console.log(firstChar); // Output: H
+console.log(fifthChar); // Output: o
