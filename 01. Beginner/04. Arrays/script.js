ec89e6d66afad4a52142bde5027f81afbe1dcfb5
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-17 22:59:51 +0600

    Array exercises

--- 01. Beginner/04. Arrays/script.js
+++ 01. Beginner/04. Arrays/script.js
@@ -12,3 +12,29 @@ var myArray = [10, 20, 30, 40, 50];
 for (var i = 0; i < myArray.length; i++) {
     console.log(myArray[i]);
 }
+
+
+//////////////////////////////// Array exercises ////////////////////////////////
+// 1
+var marks = [10, 20, 30, 40, 50.5];
+var sum = 0;
+
+for (var i = 0; i < marks.length; i++) {
+    sum += marks[i];
+}
+
+console.log("Sum:", sum);
+
+let avg = sum / marks.length;
+console.log(`Average of marks = ${avg}`);
+
+
+// 2
+let value = [30, 40, 20, 55, 78];
+let idx = 0;
+
+while (idx < value.length) {
+    console.log(`Value at index ${idx} = ${value[idx]}`);
+    idx++;
+}
+
