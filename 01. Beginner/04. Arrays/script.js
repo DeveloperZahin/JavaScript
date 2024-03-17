579c1b4ccb040c4956096b959764a82db4892ae3
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-17 22:37:15 +0600

    Looping over an Array

--- 01. Beginner/04. Arrays/script.js
+++ 01. Beginner/04. Arrays/script.js
@@ -5,3 +5,10 @@ const lastFruit = fruits[fruits.length - 1];  // Accessing by length - 1
 console.log(fruits);
 console.log("First Fruit",firstFruit);
 console.log("Last Fruit",lastFruit);
+
+
+//////////////////////////////// looping over an array ////////////////////////////////
+var myArray = [10, 20, 30, 40, 50];
+for (var i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
