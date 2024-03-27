7560de727d5f7c1f403716be883e04cf4c2810ea
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-03-27 11:31:14 +0600

    Functions

---
+++ 01. Beginner/05. Functions & Methods/script.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    console.log("Hello World!");
+    console.log("Learning JavaScript");
+}
+
+myfunction();
+
+function message(msg) { // Parameter -> input
+    console.log(msg);
+}
+
+message("Hello World!");
